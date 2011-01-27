a7cb208a9edf869fdee2c83c8bc8c8f9542ad71b
Daniels <daniel@oxos.pl>
2011-01-27 02:50:46 +0100

    Refactoring

--- test/functional/stories_controller_test.rb
+++ test/functional/stories_controller_test.rb
@@ -6,36 +6,47 @@ class StoriesControllerTest < ActionController::TestCase
     setup do
       @params = incoming_params("wojciech@example.com","daniel@example.com")
     end
-  
-    context "when story is created" do
-      setup do 
-        Story.expects(:create).with(@params['message']).returns(mock('new?'=>false))
-      end
-      
-      should "render :success" do
-        post :create, @params
-        assert_response :success
-      end
-    end
     
-    context "when story is not created" do                                    
-      context "and exception is raised" do
-        should "render response code returned from pivotal" do
-          Story.expects(:create).with(@params['message']).raises(ActiveResource::ConnectionError,"")
-          ActiveResource::ConnectionError.any_instance.stubs(:response).returns(mock("response",:code=>401))
+    
+    context "when tries to create story" do
+  
+      context "when story is created" do       
+
+        should "render :success" do
+          Story.expects(:create).with(@params['message']).returns(mock('new?'=>false))
           post :create, @params
-          assert_response 401
+          assert_response :success
         end
+        
+      end
+
+      context "when story is not created" do   
+        
+        context "and exception is raised" do
+          
+          should "render response code returned from pivotal" do
+            Story.expects(:create).with(@params['message']).raises(ActiveResource::ConnectionError,"")
+            ActiveResource::ConnectionError.any_instance.stubs(:response).returns(mock("response",:code=>401))
+            post :create, @params
+            assert_response 401
+          end
+          
+        end
+
+        context "and exception is not raised" do
+          
+          should "render status code 500" do
+            Story.expects(:create).with(@params['message']).returns(mock('new?'=>true))          
+            post :create, @params
+            assert_response 403
+          end
+          
+        end
+        
       end
       
-      context "and exception is not raised" do
-        should "render status code 500" do
-          Story.expects(:create).with(@params['message']).returns(mock('new?'=>true))          
-          post :create, @params
-          assert_response 403
-        end
-      end
     end
+               
   end
 
 end
