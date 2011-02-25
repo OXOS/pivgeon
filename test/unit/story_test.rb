1b0b908eaec72a2af0138a1f10092f5c95e19a61
Daniels <daniel@oxos.pl>
2011-02-25 14:09:44 +0100

    Refactor story test + add new contexts

--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -4,7 +4,7 @@ class StoryTest < ActiveSupport::TestCase
   
   fixtures(:all)
   
-  context "it" do
+  context "A story" do
     
     setup do
       mock_requests()
@@ -12,23 +12,11 @@ class StoryTest < ActiveSupport::TestCase
       @attrs = new_story_attrs("wojciech@example.com","daniel@example.com","12345678")      
     end
     
-    should "create new story and send send request to pivotal" do      
-      assert_false(Story.create(@attrs).new?)
-    end
-    
     should "parse incoming message" do
       message = Mail.new(@incomming_message)
       message.body = "description"
       params = {:story_type=>"chore",:name=>"Story 1", :description=>"description", :requested_by=>"wojciech@example.com", :owned_by=>"daniel@example.com", :project_id=>"147449"}
       assert_equal(params, Story.parse_message(message))
-    end
-    
-    should "set token in headers" do
-      Story.token = "12345678"
-      assert_equal '12345678', Story.headers['X-TrackerToken']
-      
-      Story.token = ''
-      assert Story.headers['X-TrackerToken'].blank?
     end
     
     should "set story owner" do
@@ -41,7 +29,44 @@ class StoryTest < ActiveSupport::TestCase
     should "get token by email" do      
       assert_equal "12345678", Story.new().send("find_user_token","wojciech@example.com")
     end
-                
+    
+    should "set token in headers" do
+      Story.token = "12345678"
+      assert_equal '12345678', Story.headers['X-TrackerToken']
+      
+      Story.token = ''
+      assert Story.headers['X-TrackerToken'].blank?
+    end
+    
+    context "when created" do
+      
+      context "by message that doesn't contain project_id in subject" do
+        
+      end
+      
+      context "by message that doesn't contain story name in subject" do
+        
+      end
+      
+      context "by message that containt subject as string not separated by ':' " do
+        
+      end
+      
+    end
+    
+    context "when updated" do
+      # not implemented yet
+    end
+    
+    context "when deleted" do
+      # not implemented yet
+    end
+    
+    
+    should "create new story and send send request to pivotal" do      
+      assert_false(Story.create(@attrs).new?)
+    end
+                                       
   end
   
   protected
