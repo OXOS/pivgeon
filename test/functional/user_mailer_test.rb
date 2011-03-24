ad7891858b22d9dd67fab5016202a6f1a8a75a27
Daniels <daniel@oxos.pl>
2011-03-25 00:00:08 +0100

    Add mailer test

--- test/functional/user_mailer_test.rb
+++ test/functional/user_mailer_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # replace this with your real tests
-  test "the truth" do
-    assert true
+  
+  fixtures :all
+
+  context "Mailer" do
+    should "send confirmation" do
+      user = users(:wojciech)
+      email =  UserMailer.registration_confirmation(user).deliver!
+      assert !ActionMailer::Base.deliveries.empty?
+      assert_equal "wojciech@example.com", email.to.first
+      assert_equal "GeePivoMailin: registration confirmation", email.subject
+      assert_match /http:\/\/geepivomailindev.heroku.com\/users\/confirm\//, email.encoded
+    end
   end
+  
 end
