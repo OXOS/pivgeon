94c928bd32f668778e9e4d514e142a7e12916d5e
Daniels <daniel@oxos.pl>
2011-04-30 12:51:24 +0200

    Update error message for wrong or blank owned_by

--- app/models/story.rb
+++ app/models/story.rb
@@ -5,11 +5,14 @@ class Story < HyperactiveResource
   self.belong_tos = [:user]
   self.skip_to_xml_for = [:user_id, :project_name, :owner_email]
   
+  HUMAN_ATTRIBUTE_NAMES = {
+    "owned_by" => "The person"
+  }
+  
   include Pivgeon::Notification
   add_notifier(StoryMailer,"created_notification")
-  
-  validates(:name, :presence=>true)
-  validates(:owned_by, :presence=>true) 
+    
+  validates(:name, :presence=>true)  
   
   def self.parse_subject(subject)
     match = subject.match(/^\s*\[(.+?)\](\s*Re:\s*|\s*re:\s*|\s*RE:\s*|\s*Fwd:\s*|\s*FWD:\s*|\s*fwd:\s*|\s*PD:\s*)?(.+)/)
@@ -18,6 +21,13 @@ class Story < HyperactiveResource
       subject[:subject] = match[3]      
     end    
   end
+  
+  def self.human_attribute_name(*args)
+    attr_name = HUMAN_ATTRIBUTE_NAMES[args[0].to_s]    
+    return attr_name if attr_name    
+    super
+  end
+
   
   def self.valid_subject_format?(subject)
     !subject.match(/^\s*\[.+?\].+/).blank?
@@ -49,11 +59,20 @@ class Story < HyperactiveResource
     "https://www.pivotaltracker.com/story/show/#{self.id}"
   end
 
-  protected
+  protected    
   
-  def before_validate
+  #TODO: validations are not well done for activeresource so this is why it looks so ugly! 
+  #It should be rewrite when it will be improved in next rails releases.
+  
+  def before_validate()
     self.owned_by = owner.person.name if owner
-    self.prefix_options[:project_id] = project.id
+    self.prefix_options[:project_id] = project.id if project
+  end
+  
+  def validate()
+    before_validate()
+    errors[:project] << "that you try to create this story for does not exist." and raise(RecordNotSaved) if self.prefix_options[:project_id].blank?
+    errors[:owned_by] << "that you try to assign to the story is not a project member." and raise(RecordNotSaved) if self.owned_by.blank?     
   end
   
 end
