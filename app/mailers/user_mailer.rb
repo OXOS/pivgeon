d3668110fb4658f979130a51ab1be386bc136630
Daniels <daniel@oxos.pl>
2011-05-23 12:55:14 +0200

    Remove unworking code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,22 +5,18 @@ class UserMailer < ActionMailer::Base
   
   def created_notification(user,error_message=nil)
     @activation_link = user.activation_code
-    mail(:to => user.email, :from => from, :reply_to => "pivgeon@pivgeon.com", :subject => "#{APP_NAME}: new user confirmation", "Sender"=>headers_)
+    mail(:to => user.email, :from => from, :reply_to => "pivgeon@pivgeon.com", :subject => "#{APP_NAME}: new user confirmation")
   end
   
   def not_created_notification(user,error_message=nil)
     @email = ( user.is_a?(User) ? user.email : user.from.first )
     @user = user
     @error_message = error_message
-    mail(:to => @email, :from => from, :reply_to => "pivgeon@pivgeon.com", :subject => "#{APP_NAME}: create new account error", "Sender"=>headers_)
+    mail(:to => @email, :from => from, :reply_to => "pivgeon@pivgeon.com", :subject => "#{APP_NAME}: create new account error")
   end
   
   def from()
     %{"PivGeon" <pivgeon@pivgeon.com>}
-  end
-  
-  def headers_()
-    "pivgeon@pivgeon.com"
   end
   
 end
