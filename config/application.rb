c6e089e804d3a7de2ef694f4173d96b834e42d11
Daniels <daniel@oxos.pl>
2011-05-23 12:25:21 +0200

    Change email to pivgeon@pivgeon.com

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,6 @@ module Geepivodev
 end
 
 #CLOUDMAILIN_EMAIL_ADDRESS = "b06e829748e4a3c9cea9@cloudmailin.net"
-CLOUDMAILIN_EMAIL_ADDRESS = "62cf42b7bc93e69c7a5b@cloudmailin.net"
+#CLOUDMAILIN_EMAIL_ADDRESS = "62cf42b7bc93e69c7a5b@cloudmailin.net"
+CLOUDMAILIN_EMAIL_ADDRESS = "pivgeon@pivgeon.com"
 APP_NAME = "PivGeon"
