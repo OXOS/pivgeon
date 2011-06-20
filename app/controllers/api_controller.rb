54d8c883c341394a3425eea12f2091e9421ccf36
Daniel <daniel@oxos.pl>
2011-06-20 13:32:51 +0200

    Testing

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,19 +1,14 @@
 class ApiController < ApplicationController
   require 'ostruct'
 
-  skip_before_filter :verify_authenticity_token  
-    
+  skip_before_filter :verify_authenticity_token
+
   before_filter :parse_message
-  before_filter :find_user  
+  before_filter :find_user
   before_filter :find_project_and_story_name
-  
+
   def create
-
-    StoryMailer.created_notification(OpenStruct.new({:from=>["daniel@oxos.pl"],:name=>'asdada',:url=>'http://wp.pl'}),"message text",{}).deliver()
-
-    #Story.send_notification(OpenStruct.new(:from=>["daniel@oxos.pl"],:name=>'asdada',:url=>'http://wp.pl'),nil,:message_id => nil, :message_subject => "Hakuna matata")
-
-    handle_exception do           
+    handle_exception do
       if direct_sent_to_cloudmailin?(@message)
         create_user(@message)
       else
@@ -21,33 +16,33 @@ class ApiController < ApplicationController
       end
     end
   end
-  
-  
+
+
   protected
-    
-  def create_user(message)    
-    attrs = User.parse_message(message)  
-    @user = User.find_or_build(attrs)      
+
+  def create_user(message)
+    attrs = User.parse_message(message)
+    @user = User.find_or_build(attrs)
     @user.save!
     render_and_send_notification()
   end
-  
-  def create_story(message)    
+
+  def create_story(message)
     attrs = {:user_id=>@user.id,
              :owner_email=>@message.to.first,
              :project_name=>@project_name,
-             :name=>@story_name,             
+             :name=>@story_name,
              :description=>params["text"]}
-    Story.token = @user.token    
-    @story = Story.new(attrs)    
+    Story.token = @user.token
+    @story = Story.new(attrs)
     @story.save!
-    render_and_send_notification()    
+    render_and_send_notification()
   end
-  
+
   def direct_sent_to_cloudmailin?(message)
     return message.to.first == CLOUDMAILIN_EMAIL_ADDRESS
   end
-  
+
   def parse_message
     to = Story.detokenize(params["to"])
     from = Story.detokenize(params["from"])
@@ -55,15 +50,15 @@ class ApiController < ApplicationController
     @message = OpenStruct.new({ :to => [to], :from => [from], :body => params["text"], :subject => params["subject"], :headers => headers})
     Rails.logger.info("\nMessage params:\n#{@message.inspect}\n\n")
   end
-  
+
   def find_project_and_story_name
-    handle_exception do       
-      unless direct_sent_to_cloudmailin?(@message)        
+    handle_exception do
+      unless direct_sent_to_cloudmailin?(@message)
           @project_name,@story_name = Story.get_project_and_story_name(@message.subject,params[:to])
       end
     end
   end
-  
+
   def find_user
     handle_exception do
       unless direct_sent_to_cloudmailin?(@message)
@@ -72,13 +67,13 @@ class ApiController < ApplicationController
       end
     end
   end
-  
+
   protected
-  
+
   def handle_exception(&block)
-    headers["Content-type"] = "text/plain" 
+    headers["Content-type"] = "text/plain"
     begin
-      block.call      
+      block.call
     rescue ActiveRecord::RecordNotSaved, ActiveRecord::RecordInvalid, RecordNotSaved
       render_and_send_notification()
     rescue ActiveResource::UnauthorizedAccess, SecurityError
@@ -89,26 +84,26 @@ class ApiController < ApplicationController
       render_and_send_notification("Server error")
     rescue => error
       render_and_send_notification("Unknown error")
-    end  
+    end
   end
-      
+
   def render_and_send_notification(error_message=nil)
     error_message.blank? ? send_notification_for_object() : send_notification_for_exception(error_message)
     render(:text => "Success", :status => 200)
-  end   
-  
+  end
+
   def send_notification_for_object()
     _class,_object = get_class_and_object()
     _class.send_notification(_object,nil,:message_id => @message.headers['Message-ID'], :message_subject => @message.subject)
   end
-  
+
   def send_notification_for_exception(error_message)
     _class,_object = get_class_and_object()
     _class.send_notification(@message,error_message,:message_id => @message.headers['Message-ID'], :message_subject => @message.subject)
   end
-  
+
   def get_class_and_object()
-    direct_sent_to_cloudmailin?(@message) ? [User,@user] : [Story,@story]    
+    direct_sent_to_cloudmailin?(@message) ? [User,@user] : [Story,@story]
   end
 
   rescue_from(Exception) do |e|
