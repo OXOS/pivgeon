35eb1cb687e230af5f32a1bd31cb4fdc2dbafd8c
Wojciech Kruszewski <Wojciech.Kruszewski@sage.com>
2011-06-20 14:20:06 +0200

    Move exception handling to rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,23 +9,11 @@ class ApplicationController < ActionController::Base
       block.call      
     rescue ActiveRecord::RecordNotSaved, ActiveRecord::RecordInvalid, RecordNotSaved
       render_and_send_notification()
-    rescue ActiveResource::UnauthorizedAccess, SecurityError
-      render_and_send_notification("Unauthorized access")
-    rescue ArgumentError
-      render_and_send_notification("Invalid data")
-    rescue ActiveResource::ServerError, ActiveResource::TimeoutError
-      render_and_send_notification("Server error")
-    rescue => error
-      render_and_send_notification("Unknown error")
     end  
   end
       
-  def render_and_send_notification(error_message=nil)
-    if error_message.blank?
-      send_notification_for_object()
-    else
-      send_notification_for_exception(error_message)
-    end
+  def render_and_send_notification
+    send_notification_for_object
     render(:text => "Error", :status => 200)
   end   
   
@@ -40,12 +28,30 @@ class ApplicationController < ActionController::Base
   end
   
   def get_class_and_object()
-    direct_sent_to_cloudmailin?(@message) ? [User,@user] : [Story,@story]    
+    direct_sent_to_cloudmailin?(@message) ? [User,@user,UserMailer] : [Story,@story,StoryMailer]
   end
 
   rescue_from(Exception) do |e|
-	#TODO: consider what to do when mailer raises error but story/user is created
-	render(:text => "Success", :status => 200)
+    error_message = case(e)
+    when ArgumentError
+      "Invalid data"
+    when ActiveResource::UnauthorizedAccess, SecurityError
+      "Unauthorized access" 
+    when ActiveResource::ServerError, ActiveResource::TimeoutError
+      "Server error"
+    else
+      "Unknown error"
+    end
+  
+    render(:text => error_message, :status => 200)
+    
+    begin
+      _class,_object = get_class_and_object()
+      _class.mailer_class.not_created_notification(@message,error_message,:message_id => @message['Message-ID'], :message_subject => @message.subject).deliver
+    #rescue
+    #  #TODO: notify us *somehow*, so that we know people are not receiving error notifications...
+    end
+
   end
 
 end
