8439da014506264ffc820c01e75afd38b0f161e5
Daniel <daniel@oxos.pl>
2011-06-20 14:22:10 +0200

    Debugging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def display_all_error_messages(object,method)
     Rails.logger.info "@@@@@@@@@@@ #{object.inspect}"
-    Rails.logger.info "@@@@@@@@@@@ #{object.errors.inspect}"
+    Rails.logger.info "@@@@@@@@@@@ #{object.errors.full_messages.inspect}"
     errors = ( method == :all ? object.errors.full_messages : object.errors[method])
     list_items = errors.map{|msg| content_tag(:li, msg) }
     list_items.join().html_safe()
