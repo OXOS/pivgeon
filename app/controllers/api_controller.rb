c5b73e858a45da792c90937fb718193585b9f431
Daniel <daniel@oxos.pl>
2011-06-21 14:03:25 +0200

    Removed leftover after the merge

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -67,51 +67,5 @@ class ApiController < ApplicationController
       end
     end
   end
-
-protected
-    
-  def create_user(message)    
-    attrs = User.parse_message(message)  
-    @user = User.find_or_build(attrs)      
-    @user.save!
-    render_and_send_notification()
-  end
-  
-  def create_story(message)    
-    attrs = {:user_id=>@user.id,
-             :owner_email=>@message.to.first,
-             :project_name=>@project_name,
-             :name=>@story_name,             
-             :description=>params[:plain]}   
-    Story.token = @user.token    
-    @story = Story.new(attrs)    
-    @story.save!
-    render_and_send_notification()    
-  end
-  
-  def direct_sent_to_cloudmailin?(message)
-    return message.to.first == CLOUDMAILIN_EMAIL_ADDRESS
-  end
-  
-  def parse_message
-      @message = Mail.new(params[:message])      
-  end
-  
-  def find_project_and_story_name
-    handle_exception do       
-      unless direct_sent_to_cloudmailin?(@message)        
-          @project_name,@story_name = Story.get_project_and_story_name(@message.subject,params[:to])
-      end
-    end
-  end
-  
-  def find_user
-    handle_exception do
-      unless direct_sent_to_cloudmailin?(@message)
-        @user = User.active.find_by_email(@message.from.first)
-        raise(SecurityError) if @user.blank?
-      end
-    end
-  end
   
 end
