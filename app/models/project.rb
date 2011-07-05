6e6a9a35dff79c8d854bc50a28d104c6bd3035a8
Daniel <daniel@oxos.pl>
2011-07-05 15:18:04 +0200

    Get project name from email address - refixing

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < PivotalItem
   self.site = "http://www.pivotaltracker.com/services/v3"
   
-  RFC_2822_EMAIL_PERMITTED_CHARACTERS = %W{! # $ % & ' * + - / = ? ^ _ ` { | } ~]}
+  RFC_2822_EMAIL_PERMITTED_CHARACTERS = /[^a-zA-Z0-9!#$\%&'*+-\/=?^_`{|}~]/
 
   def self.compare_names(name1,name2)
     str1 = standarize_name(name1)
@@ -10,7 +10,7 @@ class Project < PivotalItem
   end
   
   def self.standarize_name(name)
-    name.gsub(/[\t\s]/,'').gsub(/^[a-zA-Z0-9#{RFC_2822_EMAIL_PERMITTED_CHARACTERS.join}]/,'').downcase
+    name.gsub(/[\t\s]/,'').gsub(RFC_2822_EMAIL_PERMITTED_CHARACTERS,'').downcase
   end
   
   def self.find_project_by_name(name,token)
