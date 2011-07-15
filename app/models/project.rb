3fb6a77d1c1e8883390a2ff0db018462c238615f
Daniel <daniel@oxos.pl>
2011-07-15 17:18:19 +0200

    Removed some code for tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,19 +8,12 @@ class Project < PivotalItem
   def self.compare_names(name1,name2)
     str1 = standarize_name(name1)
     str2 = standarize_name(name2)
-
-    str1_c = Iconv.conv('UTF-8//IGNORE','UTF-8',str1)
-    str2_c = Iconv.conv('UTF-8//IGNORE','UTF-8',str2)
-    
-
     Rails.logger.info "@@@@@@ Normalized names: #{str1_c} == #{str2_c} #{str1_c==str2_c}"
-
     str1 == str2
   end
   
   def self.standarize_name(name)
-    name = name.gsub(/[\t\s]/,'')
-    #name = name.gsub(RFC_2822_EMAIL_PERMITTED_CHARACTERS,'').downcase
+    name = name.gsub(RFC_2822_EMAIL_PERMITTED_CHARACTERS,'').downcase
     name
   end
   
