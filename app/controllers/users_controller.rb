9052c838dab61a08e3cf86947960704618ceaf27
Daniel <daniel@oxos.pl>
2011-07-30 14:25:03 +0200

    Form stylization + validations
    Conflicts:
    
    	Gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,12 @@ class UsersController < ApplicationController
   end 
 
   def create
-    @user = User.create params[:user]
-    redirect_to user_path(@user)
+    @user = User.new params[:user]
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :action => :new
+    end
   end
     
   def show
