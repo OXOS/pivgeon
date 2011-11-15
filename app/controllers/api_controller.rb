040a3a32825af2d9b3980982dfe3abc815183d13
Daniel <daniel@oxos.pl>
2011-11-15 17:27:24 +0100

    Changes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -10,16 +10,21 @@ class ApiController < ApplicationController
       
       @user = User.find_by_email(@message.from)
       raise(SecurityError) if @user.blank?
-  
-      uri = URI.parse("http://book-order-pivgeon.herokuapp.com")
-      response = Net::HTTP.start(uri.host, uri.port) do |http|
-        req = Net::HTTP::Post.new("/stories/new")
-        RAILS_DEFAULT_LOGGER.info "/n/n/n/n request.raw_post: " + request.raw_post + "/n/n/n/n" 
-        req.body = request.raw_post
-        #JSON.parse( http.request(req).body )
-        response = http.request(req).body
-        RAILS_DEFAULT_LOGGER.info "/n/n/n/n" + response.inspect + "/n/n/n/n"       
-      end
+              
+      url = "http://book-order-pivgeon.herokuapp.com/stories/new"
+      response = Net::HTTP.post_form(URI.parse(url), params)
+      RAILS_DEFAULT_LOGGER.info "/n/n/n/n" + response.inspect + "/n/n/n/n"   
+      
+#    
+#      uri = URI.parse("http://book-order-pivgeon.herokuapp.com")
+#      response = Net::HTTP.start(uri.host, uri.port) do |http|
+#        req = Net::HTTP::Post.new("/stories/new")
+#        RAILS_DEFAULT_LOGGER.info "/n/n/n/n request.raw_post: " + request.raw_post + "/n/n/n/n" 
+#        req.body = request.raw_post
+#        #JSON.parse( http.request(req).body )
+#        response = http.request(req).body
+#        RAILS_DEFAULT_LOGGER.info "/n/n/n/n" + response.inspect + "/n/n/n/n"       
+#      end
       
       render(:text => "Ok", :status => 200)
   end
