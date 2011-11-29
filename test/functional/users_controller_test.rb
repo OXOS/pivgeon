2dbdec061de9fb1e9d8f42687f4d6970a8de6f88
Daniel <daniel@oxos.pl>
2011-11-29 16:05:30 +0100

    Updated tests for users

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,42 +2,39 @@ require File.expand_path(File.dirname(__FILE__))+ '/../test_helper'
 
 
 class UsersControllerTest < ActionController::TestCase
-  
+
   fixtures :all
-  
-  context "User" do
-    
-    should "activate his account" do
-      user = users(:not_activated_user)
-      User.any_instance.stubs(:check_token!).returns(nil)
-      
-      assert !user.status
-      
-      get :confirm, :id=>user.activation_code
+
+  context "A user" do
+
+    should "see 'Create user' form" do
+      user = User.new
+      User.expects(:new).returns(user)
+      get :new
       assert_response :success
-      assert assigns(:user).status      
     end
-    
-    should "see information that his account is activated" do
-      user = users(:not_activated_user)
-      User.any_instance.expects(:activate!).returns(true)
-      get :confirm, :id=>user.activation_code	  
-      assert_select "p", /Your account has been activated/
+
+    should "see confirmation" do
+      get :show, :id => 1
+      assert_response :success
     end
-    
-    should "see information that his account is not activated" do
-      user = users(:not_activated_user)
-      User.any_instance.expects(:activate!).returns(false)
-      get :confirm, :id=>user.activation_code
-      assert_select "p", /Sorry. Your account hasn't been activated./
+
+    should "create new account" do
+      user = User.new
+      User.expects(:new).returns(user)
+      user.expects(:save).returns(true)
+      @controller.expects(:user_path).with(user).returns("/show/1")
+      post :create, :email => "name@example.com", :token => "12345678"
+      assert_redirected_to "/show/1"
     end
-    
-    should "see page 404 when try to access activation page using wrong token" do   
-      get :confirm, :id=>"hakunamatata"
-      assert_response 404
-#      assert @response.body.include?("The page you were looking for doesn't exist.")
-      assert_template "public/404.html"
+
+    should "not create new account" do
+      user = User.new
+      User.expects(:new).returns(user)
+      user.expects(:save).returns(false)
+      post :create, :email => "name@example.com", :token => "12345678"
+      assert_template "/new"
     end
-    
+
   end
 end
