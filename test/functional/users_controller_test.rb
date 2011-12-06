892f39165f159f0e1c79e7955bfd02651cbc2e44
Daniel <daniel@oxos.pl>
2011-12-06 17:46:24 +0100

    Updated tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -14,27 +14,56 @@ class UsersControllerTest < ActionController::TestCase
       assert_response :success
     end
 
-    should "see confirmation" do            
-      get :show, :id => 1
-      assert_response :success
-    end
+
 
     should "create new account" do
       user = User.new
       User.expects(:new).returns(user)
-      user.expects(:save).returns(true)    
-      @controller.expects(:user_path).with(user).returns("/show/1")
+      user.expects(:save).returns(true)
       post :create, :email => "name@example.com", :token => "12345678"
-      assert_redirected_to "/show/1"    
-    end      
+      assert_template :show
+      assert_equal "Your account has been created. We require you to activate your account by email, just click the link we have sent you.", flash[:notice]
+    end
 
     should "not create new account" do
       user = User.new
       User.expects(:new).returns(user)
       user.expects(:save).returns(false)    
       post :create, :email => "name@example.com", :token => "12345678"
-      assert_template "/new"    
-    end      
-    
+      assert_template "/new"
+    end
+
+    should "activate his account" do
+      user = users(:not_activated_user)
+      User.any_instance.stubs(:check_token!).returns(nil)
+
+      assert !user.status
+
+      get :confirm, :id=>user.activation_code
+      assert_response :success
+      assert assigns(:user).status
+    end
+
+    should "see information that his account is activated" do
+      user = users(:not_activated_user)
+      User.any_instance.expects(:activate!).returns(true)
+      get :confirm, :id=>user.activation_code
+      assert_select "p", /Your account has been activated/
+    end
+
+    should "see information that his account is not activated" do
+      user = users(:not_activated_user)
+      User.any_instance.expects(:activate!).returns(false)
+      get :confirm, :id=>user.activation_code
+      assert_select "p", /Sorry. Your account hasn't been activated./
+    end
+
+    should "see page 404 when try to access activation page using wrong token" do
+      get :confirm, :id=>"hakunamatata"
+      assert_response 404
+#      assert @response.body.include?("The page you were looking for doesn't exist.")
+      assert_template "public/404.html"
+    end
+
   end
 end
